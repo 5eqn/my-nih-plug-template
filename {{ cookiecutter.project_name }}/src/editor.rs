23f98121bd43db60c1e2412c637a98e9a761cd64
5eqn <491100866@qq.com>
2024-04-18 16:11:54 +0800

    feat: switch to vizia backend to support multiline text

--- {{ cookiecutter.project_name }}/src/editor.rs
+++ {{ cookiecutter.project_name }}/src/editor.rs
@@ -1,119 +1,67 @@
 use atomic_float::AtomicF32;
-use nih_plug::prelude::{util, Editor, GuiContext};
-use nih_plug_iced::widgets as nih_widgets;
-use nih_plug_iced::*;
+use nih_plug::prelude::{util, Editor};
+use nih_plug_vizia::vizia::prelude::*;
+use nih_plug_vizia::widgets::*;
+use nih_plug_vizia::{assets, create_vizia_editor, ViziaState, ViziaTheming};
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use std::time::Duration;
 
 use crate::{{ cookiecutter.struct_name }}Params;
 
+#[derive(Lens)]
+struct Data {
+    params: Arc<{{ cookiecutter.struct_name }}Params>,
+    peak_meter: Arc<AtomicF32>,
+}
+
+impl Model for Data {}
+
 // Makes sense to also define this here, makes it a bit easier to keep track of
-pub(crate) fn default_state() -> Arc<IcedState> {
-    IcedState::from_size(200, 150)
+pub(crate) fn default_state() -> Arc<ViziaState> {
+    ViziaState::new(|| (200, 150))
 }
 
 pub(crate) fn create(
     params: Arc<{{ cookiecutter.struct_name }}Params>,
     peak_meter: Arc<AtomicF32>,
-    editor_state: Arc<IcedState>,
+    editor_state: Arc<ViziaState>,
 ) -> Option<Box<dyn Editor>> {
-    create_iced_editor::<{{ cookiecutter.struct_name }}Editor>(editor_state, (params, peak_meter))
-}
+    create_vizia_editor(editor_state, ViziaTheming::Custom, move |cx, _| {
+        assets::register_noto_sans_light(cx);
+        assets::register_noto_sans_thin(cx);
 
-struct {{ cookiecutter.struct_name }}Editor {
-    params: Arc<{{ cookiecutter.struct_name }}Params>,
-    context: Arc<dyn GuiContext>,
-
-    peak_meter: Arc<AtomicF32>,
-
-    gain_slider_state: nih_widgets::param_slider::State,
-    peak_meter_state: nih_widgets::peak_meter::State,
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    /// Update a parameter's value.
-    ParamUpdate(nih_widgets::ParamMessage),
-}
-
-impl IcedEditor for {{ cookiecutter.struct_name }}Editor {
-    type Executor = executor::Default;
-    type Message = Message;
-    type InitializationFlags = (Arc<{{ cookiecutter.struct_name }}Params>, Arc<AtomicF32>);
-
-    fn new(
-        (params, peak_meter): Self::InitializationFlags,
-        context: Arc<dyn GuiContext>,
-    ) -> (Self, Command<Self::Message>) {
-        let editor = {{ cookiecutter.struct_name }}Editor {
-            params,
-            context,
-
-            peak_meter,
-
-            gain_slider_state: Default::default(),
-            peak_meter_state: Default::default(),
-        };
-
-        (editor, Command::none())
-    }
-
-    fn context(&self) -> &dyn GuiContext {
-        self.context.as_ref()
-    }
-
-    fn update(
-        &mut self,
-        _window: &mut WindowQueue,
-        message: Self::Message,
-    ) -> Command<Self::Message> {
-        match message {
-            Message::ParamUpdate(message) => self.handle_param_message(message),
+        Data {
+            params: params.clone(),
+            peak_meter: peak_meter.clone(),
         }
+        .build(cx);
 
-        Command::none()
-    }
+        VStack::new(cx, |cx| {
+            Label::new(cx, "{{ cookiecutter.struct_name }} GUI")
+                .font_family(vec![FamilyOwned::Name(String::from(assets::NOTO_SANS))])
+                .font_weight(FontWeightKeyword::Thin)
+                .font_size(30.0)
+                .height(Pixels(50.0))
+                .child_top(Stretch(1.0))
+                .child_bottom(Pixels(0.0));
 
-    fn view(&mut self) -> Element<'_, Self::Message> {
-        Column::new()
-            .align_items(Alignment::Center)
-            .push(
-                Text::new("{{ cookiecutter.struct_name }} GUI")
-                    .font(assets::NOTO_SANS_LIGHT)
-                    .size(40)
-                    .height(50.into())
-                    .width(Length::Fill)
-                    .horizontal_alignment(alignment::Horizontal::Center)
-                    .vertical_alignment(alignment::Vertical::Bottom),
-            )
-            .push(
-                Text::new("{{ cookiecutter.struct_name }}")
-                    .height(20.into())
-                    .width(Length::Fill)
-                    .horizontal_alignment(alignment::Horizontal::Center)
-                    .vertical_alignment(alignment::Vertical::Center),
-            )
-            .push(
-                nih_widgets::ParamSlider::new(&mut self.gain_slider_state, &self.params.gain)
-                    .map(Message::ParamUpdate),
-            )
-            .push(Space::with_height(10.into()))
-            .push(
-                nih_widgets::PeakMeter::new(
-                    &mut self.peak_meter_state,
-                    util::gain_to_db(self.peak_meter.load(std::sync::atomic::Ordering::Relaxed)),
-                )
-                .hold_time(Duration::from_millis(600)),
-            )
-            .into()
-    }
+            Label::new(cx, "{{ cookiecutter.struct_name }}");
+            ParamSlider::new(cx, Data::params, |params| &params.gain);
 
-    fn background_color(&self) -> nih_plug_iced::Color {
-        nih_plug_iced::Color {
-            r: 0.98,
-            g: 0.98,
-            b: 0.98,
-            a: 1.0,
-        }
-    }
+            PeakMeter::new(
+                cx,
+                Data::peak_meter
+                    .map(|peak_meter| util::gain_to_db(peak_meter.load(Ordering::Relaxed))),
+                Some(Duration::from_millis(600)),
+            )
+            // This is how adding padding works in vizia
+            .top(Pixels(10.0));
+        })
+        .row_between(Pixels(0.0))
+        .child_left(Stretch(1.0))
+        .child_right(Stretch(1.0));
+
+        ResizeHandle::new(cx);
+    })
 }
